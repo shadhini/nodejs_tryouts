b447d178e423354a6f9a57ce4a28587b07c3d3f5
shadhini <jshadhiniaseka@gmail.com>
2019-11-11 16:05:09 +0530

    node js starting point --> app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// #### app.js ####
+// the starting point
+// whenever we run a nodeJS application, our program is gonna look into this file first.
+// whatever code is in here,  it's gonna run
+// this is like our main file
+console.log('Hello! NodeJS');
+
+// whenever you typing JavaScript, It's just pretty much the code that runs in the browser
+// unless you have a while loop or something it runs, it just completes and then it's done
+// however think of this code right here as a code that gonna run on a server
+// any nodeJS code gonna run on a server and it's gonna run continuously, so this process is going on, until we stop it
+// all these code gonna run on your server side code
+
