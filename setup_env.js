a3908be56cc7ec975eb4670f2002c8c6e5884333
shadhini <jshadhiniaseka@gmail.com>
2019-11-11 16:04:51 +0530

    setup webstorm execution environment

---
+++ setup_env.js
@@ -0,0 +1,9 @@
+// whenever we start running actual applications,
+// most of the times, we wanna make these for websites, rather than a user using a terminal or command prompt
+// #### Setting up browser ####
+// 1. go to drop down menu of runnables, and click "Edit Configurations..."
+// 2. go to "Browser/ Live Edit" tab
+// 3. tick the check box "After Launch" and
+//    select the brwser in which you wanna test this application from the drop down menu
+// 4. set a URL (e.g. http://localhost:8888)
+// 5. click APPLY --> OK
