d17807ba65379fe9ccbe8738920b13ee0776a994
shadhini <jshadhiniaseka@gmail.com>
2019-11-14 17:25:14 +0530

    EJS

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' }); // renders a view, in this case, views/index.ejs
+  res.render('index', {
+      title: 'Express',
+      name: 'Shadhini'
+  }); // renders a view, in this case, views/index.ejs
 });
 
 module.exports = router;
